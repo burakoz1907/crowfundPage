c46e658c4574991b3e55bec8443c4dfd393ba119
Burak <ozb5076@gmail.com>
2024-04-04 09:00:33 +0300

    first commit

---
+++ assets/js/script.js
@@ -0,0 +1,8 @@
+const modalAc=document.querySelector(".btn-1");
+const modal=document.querySelector("#modal");
+ 
+        modalAc.addEventListener("click",function(){
+            
+            modal.showModal();
+        });
+ 
